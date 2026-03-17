6d296bc907f8d651868b971027c5bd74bd9947e8
Matt Diestelhorst <diestelhorst9811@uwlax.edu>
2026-03-16 20:07:41 -0500

    use environment variables for cloudinary

--- backend/src/main/java/com/tesl/backend/CardService.java
+++ backend/src/main/java/com/tesl/backend/CardService.java
@@ -1,56 +1,79 @@
 package com.tesl.backend;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
 @Service
 public class CardService {
 
-    private static final String TESL_PATH = "/home/matt/TESL/TESL-web";
     private static final String CLOUDINARY_BASE = "https://res.cloudinary.com/dmwxzhylv/image/upload/TESL";
+    private static final String[] SINGLE_COLORS = {"Blue", "Green", "Red", "Yellow", "Purple", "Neutral"};
+    private static final String[] MULTI_COLORS = {
+        "Blue_Green", "Blue_Purple", "Blue_Yellow", "Blue_Yellow_Green",
+        "Blue_Yellow_Purple", "Blue_Green_Purple", "Red_Blue", "Red_Blue_Green",
+        "Red_Green", "Red_Green_Purple", "Red_Yellow", "Red_Yellow_Green",
+        "Red_Yellow_Purple", "Yellow_Green", "Yellow_Green_Purple", "Yellow_Purple"
+    };
+
+    private final Cloudinary cloudinary;
+
+    public CardService() {
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
+            "api_key", System.getenv("CLOUDINARY_API_KEY"),
+            "api_secret", System.getenv("CLOUDINARY_API_SECRET")
+        ));
+    }
 
     public List<Card> getAllCards() {
         List<Card> cards = new ArrayList<>();
 
-        // Handle single color folders
-        String[] singleColors = {"Blue", "Green", "Red", "Yellow", "Purple", "Neutral"};
-        for (String color : singleColors) {
-            File frontsFolder = new File(TESL_PATH + "/" + color + "/fronts");
-            if (frontsFolder.exists()) {
-                File[] images = frontsFolder.listFiles((dir, name) -> name.endsWith(".png"));
-                if (images != null) {
-                    for (File image : images) {
-                        String filename = image.getName().replace(".png", "");
+        // Single colors
+        for (String color : SINGLE_COLORS) {
+            try {
+                Map result = cloudinary.api().resources(ObjectUtils.asMap(
+                    "type", "upload",
+                    "prefix", "TESL/" + color + "/fronts/",
+                    "max_results", 500
+                ));
+                List<Map> resources = (List<Map>) result.get("resources");
+                if (resources != null) {
+                    for (Map resource : resources) {
+                        String publicId = (String) resource.get("public_id");
+                        String filename = publicId.substring(publicId.lastIndexOf("/") + 1);
                         String id = color + "_" + filename;
                         String imageUrl = CLOUDINARY_BASE + "/" + color + "/fronts/" + filename;
                         cards.add(new Card(id, imageUrl, List.of(color)));
                     }
                 }
+            } catch (Exception e) {
+                System.out.println("Error fetching " + color + ": " + e.getMessage());
             }
         }
 
-        // Handle multi color folders
-        File multiColorFolder = new File(TESL_PATH + "/Multi Color");
-        if (multiColorFolder.exists()) {
-            File[] colorCombos = multiColorFolder.listFiles(File::isDirectory);
-            if (colorCombos != null) {
-                for (File combo : colorCombos) {
-                    String comboName = combo.getName();
-                    List<String> colors = Arrays.asList(comboName.split("_"));
-                    File[] images = combo.listFiles((dir, name) -> name.endsWith(".png"));
-                    if (images != null) {
-                        for (File image : images) {
-                            String filename = image.getName().replace(".png", "");
-                            String id = comboName + "_" + filename;
-                            String imageUrl = CLOUDINARY_BASE + "/Multi Color/" + comboName + "/" + filename;
-                            cards.add(new Card(id, imageUrl, colors));
-                        }
+        // Multi colors
+        for (String combo : MULTI_COLORS) {
+            try {
+                Map result = cloudinary.api().resources(ObjectUtils.asMap(
+                    "type", "upload",
+                    "prefix", "TESL/Multi Color/" + combo + "/",
+                    "max_results", 500
+                ));
+                List<Map> resources = (List<Map>) result.get("resources");
+                if (resources != null) {
+                    List<String> colors = Arrays.asList(combo.split("_"));
+                    for (Map resource : resources) {
+                        String publicId = (String) resource.get("public_id");
+                        String filename = publicId.substring(publicId.lastIndexOf("/") + 1);
+                        String id = combo + "_" + filename;
+                        String imageUrl = CLOUDINARY_BASE + "/Multi Color/" + combo + "/" + filename;
+                        cards.add(new Card(id, imageUrl, colors));
                     }
                 }
+            } catch (Exception e) {
+                System.out.println("Error fetching " + combo + ": " + e.getMessage());
             }
         }
 
